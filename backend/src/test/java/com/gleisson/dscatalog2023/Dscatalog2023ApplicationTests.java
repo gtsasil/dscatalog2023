016fdb2da00ca0dfa467ca24447cc2f7d8db2a69
Gleisson Santos Silva <gtsasil@gmail.com>
2023-09-03 13:48:14 +0100

    backend project created

---
+++ backend/src/test/java/com/gleisson/dscatalog2023/Dscatalog2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gleisson.dscatalog2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dscatalog2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
